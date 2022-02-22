7392e7596f3bc4620f33e0b55eadcb90348b91ee
Kajanth <kajanthkumar@gmail.com>
2022-02-22 16:44:41 -0500

    display user sent attachments for ui

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Typography } from "@material-ui/core";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexDirection: "column",
@@ -24,18 +24,67 @@ const useStyles = makeStyles(() => ({
   bubble: {
     background: "#F4F6FA",
     borderRadius: "10px 10px 0 10px"
+  },
+  singleImage: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "150px",
+    height: "173px",
+    borderRadius: "10px 10px 0 10px",
+    overflow: "hidden",
+    "& img": {
+      maxWidth: "100%"
+    }
+  },
+  multiImageContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: theme.spacing(4.5),
+  },
+  multiImage: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "104px",
+    height: "90px",
+    marginLeft: theme.spacing(3),
+    borderRadius: "10px 10px 0 10px",
+    overflow: "hidden",
+    "& img": {
+      maxWidth: "100%"
+    }
   }
 }));
 
 const SenderBubble = (props) => {
   const classes = useStyles();
-  const { time, text } = props;
+  const { time, text, attachments } = props;
   return (
     <Box className={classes.root}>
-      <Typography className={classes.date}>{time}</Typography>
+      {
+        attachments.length !== 1 && <Typography className={classes.date}>{time}</Typography>
+      }
       <Box className={classes.bubble}>
+        {
+          (attachments && attachments.length === 1) && <Box className={classes.singleImage}>
+            <img src={attachments[0]}/>
+          </Box>
+        }
         <Typography className={classes.text}>{text}</Typography>
       </Box>
+      {
+        attachments && attachments.length > 1 && <Box className={classes.multiImageContainer}>
+          {
+            attachments.map(attachement => <Box className={classes.multiImage}>
+              <img src={attachement}/>
+            </Box>)
+          }
+        </Box>
+      }
+      {
+        attachments.length > 1 && <Typography className={classes.date}>{time}</Typography>
+      }
     </Box>
   );
 };
