d0f0be46edae4c7de4ab30162779446cbb46acb2
Kajanth <kajanthkumar@gmail.com>
2022-02-23 08:36:22 -0500

    compiler warnings

--- client/src/components/ActiveChat/OtherUserBubble.js
+++ client/src/components/ActiveChat/OtherUserBubble.js
@@ -76,7 +76,7 @@ const OtherUserBubble = (props) => {
         <Box className={classes.bubble}>
           {
             (attachments && attachments.length === 1) && <Box className={classes.singleImage}>
-              <img src={attachments[0]}/>
+              <img src={attachments[0]} alt={""}/>
             </Box>
           }
           {
@@ -89,7 +89,7 @@ const OtherUserBubble = (props) => {
           <Box className={classes.multiImageContainer}>
             {
               attachments.map(attachement => <Box className={classes.multiImage}>
-                <img src={attachement}/>
+                <img src={attachement} alt={""}/>
               </Box>)
             }
           </Box>
--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -70,7 +70,7 @@ const SenderBubble = (props) => {
       <Box className={classes.bubble}>
         {
           (attachments && attachments.length === 1) && <Box className={classes.singleImage}>
-            <img src={attachments[0]}/>
+            <img src={attachments[0]} alt={""}/>
           </Box>
         }
         {
@@ -81,7 +81,7 @@ const SenderBubble = (props) => {
         attachments && attachments.length > 1 && <Box className={classes.multiImageContainer}>
           {
             attachments.map(attachement => <Box className={classes.multiImage}>
-              <img src={attachement}/>
+              <img src={attachement} alt={""}/>
             </Box>)
           }
         </Box>
